35f93c4524c735b76e18b34b82f8ef2df62f3ae4
ZFX <2675699284@qq.com>
2020-02-21 22:17:22 +0800

    Add files via upload

---
+++ login/setup.sh
@@ -0,0 +1,24 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg install python
+cp login.py $PREFIX/bin/
+chmod 700 $PREFIX/bin/login.py
+cp login $PREFIX/bin/
+chmod 700 $PREFIX/bin/login
+mkdir /data/data/com.termux/files/usr/share/login/
+clear
+
+#TODO: hide input
+read -p "输入新密码: " passone;
+read -p "请重复输入密码: " passtwo;
+
+if [ $passone = $passtwo ];
+then
+	touch /data/data/com.termux/files/usr/share/login/.pass
+	python -c "import hashlib; print(hashlib.sha1(b'$passone').hexdigest())" > /data/data/com.termux/files/usr/share/login/.pass
+	echo '已安装登录。'
+
+elif [ $passone != $passtwo ];
+then
+	echo '密码不匹配'
+fi
