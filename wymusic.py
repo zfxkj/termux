f72a390b4f403aef816db9005e29468c337ddab6
ZFX <2675699284@qq.com>
2020-02-22 12:25:11 +0800

    网易云音乐批量下载

---
+++ wymusic.py
@@ -0,0 +1,33 @@
+import requests
+import re
+headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36'
+}
+urlaq = input('请入链接：')
+url_fg = 'https://music.163.com/playlist?id='
+id_fx = urlaq.split('=')[-1]
+url = url_fg + id_fx
+#url = urlaq.replace('/#','')
+url1 = 'http://music.163.com/song/media/outer/url?id='
+qq = requests.get(url, headers=headers).text
+ids = re.findall('<ul class="f-hide">.*?</ul>', qq)
+for ids2 in ids:
+    ids1 = re.findall('<a href="/song\?id=([0-9]{8,14})">(.{4,30})</a>', ids2)
+    for qqq in ids1:
+        name = qqq[1]+'.mp3'
+        sumx = qqq[0]
+        url2 = url1 + sumx + '.mp3'
+        print(url2)
+        
+        try:
+            music = requests.get(url2).content
+            with open('ZFX/' + name, 'wb')as f:
+                f.write(music)
+        except Exception as e:
+            print('错误:',e)
+        else:
+            print("下载成功～～")
+"""
+本程序由ZFX编辑，
+转载请标明出处！
+"""
