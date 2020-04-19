1a1bd937b7306b4a462bff80df59a970a2e74d25
ZFX <2675699284@qq.com>
2020-04-19 22:05:48 +0800

    Update zfx.sh

--- zfx.sh
+++ zfx.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 if [ -e "$PREFIX/etc/lyn" ];then
-bash $PREFIX/etc/lyn
+sh $PREFIX/etc/lyn
 else
 if [ -e "$PREFIX/bin/curl" ];then
 curl -o lyn https://raw.githubusercontent.com/ZFXLYN/termux/master/lyn.sh
 mv -f lyn $PREFIX/etc/
 chmod +x $PREFIX/etc/lyn
-bash $PREFIX/etc/lyn
+sh $PREFIX/etc/lyn
 else
 pkg install -y curl
 curl -o lyn https://raw.githubusercontent.com/ZFXLYN/termux/master/lyn.sh
 mv -f lyn $PREFIX/etc/
 chmod +x $PREFIX/etc/lyn
-bash $PREFIX/etc/lyn
+sh $PREFIX/etc/lyn
 fi
 fi
